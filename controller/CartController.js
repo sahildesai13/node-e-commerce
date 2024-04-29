a83be8606f26f42dc3ac9a12effc81687cc87ef9
sahildesai13 <sahildesai7338@gmail.com>
2024-04-29 11:33:02 +0530

    cart section

--- controller/CartController.js
+++ controller/CartController.js
@@ -7,42 +7,38 @@ storage.init();
 
 exports.addToCart = async (req, res) => {
     let userId = await storage.getItem('userId');
-    console.log(userId);
+    let productId = req.params.id;
     if (userId) {
-        let productId = req.params.id;
         let existing = await Cart.findOne({ userId, "product.productId": productId });
         if (existing) {
-            console.log(existing.id);
-            var quantity = existing.product.forEach((ele) => {
-                if (ele.productId === productId) {
-                    return ele.quantity;
-                }
-            });
-            
-            var updateditem = await Cart.findByIdAndUpdate({id: existing.id ,})
-        //    var productItem = existing.product.forEach((ele) => {
-        //         if (ele.productId === productId) {
-        //             ele.quantity++;
-        //         }
-        //     });
-                await existing.save();
-            res.status(201).json({
-                message: "Quantity has been updated",
-                cart: existing
-            });
+            await Cart.findOneAndUpdate( { userId, "product.productId": productId }, { $inc: { "product.$.quantity": 1 } }, { new: true });
+            let cartData = await Cart.findOne({ userId, "product.productId": productId })
+            res.status(200).json({
+                message:"data",
+                cart:cartData
+            })
         }
-         else {
+        else {
             let DataProduct = await Product.findById(productId);
             console.log(DataProduct);
             if (DataProduct) {
-                let cart = new Cart({ userId, product: [] });
-                cart.product.push({productId: productId, products:DataProduct, quantity:1});
-                await cart.save();
-                console.log(cart);
-                res.status(201).json({
-                    message: "Product added to cart successfully",
-                    cart: cart
-                });
+                let user = await Cart.findOne({ userId });
+                if (user) {
+                    await Cart.updateOne({ userId }, { $push: { product: { productId: productId, products: DataProduct, quantity: 1 } } });
+                    let updatedCart = await Cart.findOne({ userId });
+                    res.status(201).json({
+                        message: "Product added to cart successfully",
+                        cart: updatedCart
+                    });
+                } else {
+                    let cart = new Cart({ userId, product: [] });
+                    cart.product.push({ productId: productId, products: DataProduct, quantity: 1 });
+                    await cart.save();
+                    res.status(201).json({
+                        message: "Product added to cart successfully",
+                        cart: cart
+                    });
+                }
             } else {
                 res.status(200).json({
                     message: "Product Not Found"
